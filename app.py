668a1e5e0ba1be4909f5ecac91ae3d43d8d093fd
GXF0102 <811679259@qq.com>
2019-12-27 11:40:19 +0800

    Add index page.

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 app=Flask(__name__)
 
+# @app.route('/')
+# @app.route('/index')
+# @app.route('/home')
+# def hello():
+# 	return u"欢迎来的我的世界 watchlist！"
+# 	#return '<h1>Hello Totoro!</h1><img src="http://hellflask.com/totoro.gif">'
+
+# @app.route('/user/<name>')
+# def user_pagge(name):
+# 	return 'User: %s' % name
+
+# @app.route('/test')
+# def test_url_for():
+# 	print(url_for("hello"))
+# 	print(url_for("user_pagge",name="greyli"))
+# 	print(url_for("user_pagge","peter"))
+# 	print(url_for('test_url_for'))
+# 	print(url_for('test_url_for',num=2))
+# 	return 'Test Page'
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-@app.route('/index')
-@app.route('/home')
-def hello():
-	return u"欢迎来的我的世界 watchlist！"
-	#return '<h1>Hello Totoro!</h1><img src="http://hellflask.com/totoro.gif">'
-
-@app.route('/user/<name>')
-def user_pagge(name):
-	return 'User: %s' % name
-
-@app.route('/test')
-def test_url_for():
-	print(url_for("hello"))
-	print(url_for("user_pagge",name="greyli"))
-	print(url_for("user_pagge","peter"))
-	print(url_for('test_url_for'))
-	print(url_for('test_url_for',num=2))
-	return 'Test Page'
-
-
+def index():
+	return render_template('index.html',name=name,movies=movies)
